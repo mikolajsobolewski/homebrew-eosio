d828b770e72409059f97463ead8dc3b6174ed702
Mikolaj Sobolewksi <mikolaj@meganuke.eu>
2021-02-13 17:01:51 +0300

    Merge eosio and cdt into the single tap also added versioned formulae

---
+++ Formula/eosio.cdt@1.6.rb
@@ -0,0 +1,32 @@
+class EosioCdt16 < Formula
+
+   homepage "https://github.com/eosio/eosio.cdt"
+   revision 0
+   url "https://github.com/eosio/eosio.cdt/archive/v1.6.3.tar.gz"
+   version "1.6.3"
+   
+   option :universal
+   keg_only :versioned_formula
+
+   depends_on "cmake" => :build
+   depends_on "automake" => :build
+   depends_on "libtool" => :build
+   depends_on "wget" => :build
+   depends_on "gmp" => :build
+   depends_on "gettext" => :build
+   depends_on "doxygen" => :build
+   depends_on "graphviz" => :build
+   depends_on "lcov" => :build
+   depends_on :xcode => :build
+   depends_on :macos => :high_sierra
+   depends_on :arch =>  :intel
+  
+   bottle do
+      root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.6.3"
+      sha256 "0276755dc0a444a9dffeb1aeddb26246faf0464daf7e2e133caaf96097bfa7c7" => :mojave
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
+end
+__END__
---
+++ Formula/eosio@2.1.rb
@@ -0,0 +1,26 @@
+class Eosio21 < Formula
+
+   homepage "https://github.com/eosio/eos"
+   revision 0
+   url "https://github.com/eosio/eos/archive/v2.1.0-rc2.tar.gz"
+   version "2.1.0-rc2"
+
+   option :universal
+   keg_only :versioned_formula
+
+   depends_on "gmp"
+   depends_on "gettext"
+   depends_on "openssl@1.1"
+   depends_on "libusb"
+   depends_on :macos => :mojave
+   depends_on :arch =>  :intel
+
+   bottle do
+      root_url "https://github.com/eosio/eos/releases/download/v2.1.0-rc2"
+      sha256 "9f82af896a011adb145918726f2b18e2fbbe77a310b99d227dde5614366e2707" => :mojave
+   end
+   def install
+      raise "Error, only supporting binary packages at this time"
+   end
+end
+__END__
